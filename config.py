a4a99af3636bda14692dca48b5386c00e11b003c
dieginin <diegobalestra9@gmail.com>
2023-09-18 01:59:57 -0400

    carga de cogs automática

--- config.py
+++ config.py
@@ -5,5 +5,3 @@ dot = dotenv_values()
 TOKEN_API = dot["BRAWL_API"]
 TOKEN_BOT = dot["BOT_API"]
 ROYALE_URL = "https://bsproxy.royaleapi.dev/v1/"
-
-COGS = ["commands.extras", "commands.club"]
--- functions/cog_loader.py
+++ functions/cog_loader.py
@@ -1,13 +1,19 @@
+import os
+
 from colorama import Fore
 from discord.ext import commands
 
-from config import COGS
-
 
 async def cog_loader(bot: commands.Bot):
-    for cog in COGS:
-        try:
-            await bot.load_extension(cog)
-        except:
-            pass
-        print(Fore.MAGENTA + "COG" + Fore.RESET + f" {cog} cargados")
+    for filename in os.listdir("./commands"):
+        if filename.endswith(".py"):
+            try:
+                await bot.load_extension(f"commands.{filename[:-3]}")
+                print(
+                    Fore.MAGENTA
+                    + "COG"
+                    + Fore.RESET
+                    + f" commands.{filename[:-3]} cargados"
+                )
+            except:
+                pass
