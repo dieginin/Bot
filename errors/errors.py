334ddf23315d549016a0922293ff588f66762b55
dieginin <diegobalestra9@gmail.com>
2023-09-19 16:44:13 -0400

    mensaje se elimina solo y se agrego handle a invoke error

--- errors/errors.py
+++ errors/errors.py
@@ -6,8 +6,10 @@ async def on_error(interaction: discord.Interaction, error):
     embed = discord.Embed(title="Error", description="", color=discord.Color.dark_red())
 
     if isinstance(error, app_commands.CheckFailure):
-        embed.description = f"Primero debes lanzar el comando **`/vincular`** para vincular tu perfil\nNo posees nick o tu nick no pertenece al club"
+        embed.description = "Primero debes lanzar el comando **`/vincular`** para vincular tu perfil\nNo posees nick o tu nick no pertenece al club"
+    elif isinstance(error, app_commands.CommandInvokeError):
+        embed.description = "Tuve un :dash:, perdón\nIntentalo de nuevo"
     else:
         raise error
 
-    await interaction.response.send_message(embed=embed, ephemeral=True)
+    await interaction.response.send_message(embed=embed, ephemeral=True, delete_after=5)
